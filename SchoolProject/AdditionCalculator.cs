d18ad57f9c4e741a7ff3d5b67b36c1ae6363c118
Aleksandra Krasteva <I476232@fhict.nl>
2023-05-03 00:14:47 +0200

    initial commit

---
+++ SchoolProject/AdditionCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject
+{
+   public class AdditionCalculator
+    {
+        public int Num1 { get; private set; }
+        public int Num2 { get; private set; }
+        public int Answer { get; private set; }
+
+        public AdditionCalculator()
+        {
+
+        }
+
+
+        public string Generate()
+        {
+            Random rand = new();
+
+            Num1 = rand.Next(1, 10);
+            Num2 = rand.Next(1, 10);
+
+            Answer = Num1 + Num2;
+
+            return $"Addition: What's {Num1} + {Num2}?";
+        }
+    }
+}
---
+++ SchoolProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SchoolProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdditionCalculator additionCalculator = new();
+            const int ADDITION = 1;
+            const int SUBTRACTION = 2;
+            const int MULT = 3;
+            int playerAns = 0;
+
+
+
+            Console.WriteLine("Pre-school caculator.");
+            Console.WriteLine("Please enter a number corresponding to the category");
+            Console.WriteLine("1. Addition \n" +
+                "2.Subtraction \n" +
+                "3.Multiplication");
+            int choice;
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == 1)
+            {
+                Console.WriteLine("Addition selected.");
+                string problem = additionCalculator.Generate();
+                
+                while (playerAns != additionCalculator.Answer)
+                {
+                    Console.WriteLine(problem);
+                    playerAns = Convert.ToInt32(Console.ReadLine());
+                    if(playerAns != additionCalculator.Answer)
+                    {
+                        Console.WriteLine("Wrong answer, try again.");
+                    }
+                    
+                }
+
+                Console.WriteLine($"Correct, the answer is indeed {additionCalculator.Answer} \n" +
+                    $"Would you like to solve another problem, or quit the game?");
+
+                   
+            }
+            if(choice == 2)
+            {
+                Console.WriteLine("Subtraction selected.");
+                
+            }
+            if(choice == 3)
+            {
+                Console.WriteLine("Multiplication selected.");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
